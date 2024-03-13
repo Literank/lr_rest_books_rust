a5c02d2ab89596c3c1e8c3c31ea973af0c8b4a44
netdong-li <east2050@gmail.com>
2024-03-13 23:54:13 +0800

    Initial version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::content;
+
+#[get("/ping")]
+fn ping() -> content::RawJson<&'static str> {
+    content::RawJson("{\"message\": \"pong\"}")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![ping])
 }
