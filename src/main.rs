8d504fa0cfa560fb162f550d39ba5b33e7e65275
netdong-li <east2050@gmail.com>
2024-03-20 23:33:08 +0800

    Custom port

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod adapter;
 mod application;
 mod domain;
 mod infrastructure;
+
 use crate::adapter::router::*;
 use crate::infrastructure::parse_config;
 
@@ -15,22 +16,25 @@ fn rocket() -> _ {
     let c = parse_config(CONFIG_FILE);
     let wire_helper = application::WireHelper::new(&c).expect("Failed to create WireHelper");
     let r = adapter::make_router(&wire_helper);
-    rocket::build().manage(r).mount(
-        "/",
-        routes![
-            health_check,
-            get_books,
-            get_book,
-            create_book,
-            update_book,
-            delete_book,
-            get_reviews_of_book,
-            get_review,
-            create_review,
-            update_review,
-            delete_review,
-            user_sign_up,
-            user_sign_in
-        ],
-    )
+    rocket::build()
+        .configure(rocket::Config::figment().merge(("port", c.app.port)))
+        .manage(r)
+        .mount(
+            "/",
+            routes![
+                health_check,
+                get_books,
+                get_book,
+                create_book,
+                update_book,
+                delete_book,
+                get_reviews_of_book,
+                get_review,
+                create_review,
+                update_review,
+                delete_review,
+                user_sign_up,
+                user_sign_in
+            ],
+        )
 }
